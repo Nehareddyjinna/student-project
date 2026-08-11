0c9fa0502f6a38e44c384e30526cd045c02b19f0
Nehareddyjinna <jinnanehareddy@gmail.com>
2026-08-11 12:49:16 +0530

    update greeting msg

--- src/student_project/main.py
+++ src/student_project/main.py
@@ -1,5 +1,5 @@
 def greet(name: str) -> str:
- return f"Hello, {name}!"
+ return f"Hello, {name}! Welcome to python"
 
 if __name__ == "__main__":
- print(greet("Student"))
+ print(greet("Jinna Neha"))
